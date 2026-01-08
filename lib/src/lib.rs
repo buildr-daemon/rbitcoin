075d5394c7bb3980666e43a6649fd79c81f54c7b
Amrit Prakash <amr1t.pr4k4sh@gmail.com>
2026-01-08 13:14:36 +0530

    blocks implemented

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,10 +1,13 @@
+use serde::{Deserialize, Serialize};
 use uint::construct_uint;
 
 construct_uint! {
+    #[derive(Serialize, Deserialize)]
     pub struct U256(4);
 }
 
 pub mod crypto;
+pub mod error;
 pub mod sha256;
 pub mod types;
 pub mod util;
--- lib/src/types.rs
+++ lib/src/types.rs
@@ -1,16 +1,68 @@
+use crate::crypto::{PublicKey, Signature};
+use crate::error::{BtcError, Result};
+use crate::sha256::Hash;
+use crate::util::MerkleRoot;
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use uuid::Uuid;
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Blockchain {
+    pub utxos: HashMap<Hash, TransactionOutput>,
     pub blocks: Vec<Block>,
 }
 
 impl Blockchain {
     pub fn new() -> Self {
-        Blockchain { blocks: vec![] }
+        Blockchain {
+            utxos: HashMap::new(),
+            blocks: vec![],
+        }
     }
-    pub fn add_block(&mut self, block: Block) {
+    pub fn add_block(&mut self, block: Block) -> Result<()> {
+        if self.blocks.is_empty() {
+            if block.header.prev_block_hash != Hash::zero() {
+                println!("Zero hash");
+                return Err(BtcError::InvalidBlock);
+            }
+        } else {
+            let last_block = self.blocks.last().unwrap();
+            if block.header.prev_block_hash != last_block.hash() {
+                println!("prev hash is wrong");
+                return Err(BtcError::InvalidBlock);
+            }
+            if !block.header.hash().matches_target(block.header.target) {
+                println!("does not match target");
+                return Err(BtcError::InvalidBlock);
+            }
+            let calculated_merkle_root = MerkleRoot::calculate(&block.transactions);
+            if calculated_merkle_root != block.header.merkle_root {
+                println!("Invalid merkle root");
+                return Err(BtcError::InvalidMerkleRoot);
+            }
+            if block.header.timestamp <= last_block.header.timestamp {
+                return Err(BtcError::InvalidBlock);
+            }
+            block.verify_transactions(self.block_height(), &self.utxos)
+        }
         self.blocks.push(block);
+        Ok(())
+    }
+    pub fn rebuild_utxos(&mut self) {
+        for block in &self.blocks {
+            for transaction in &block.transactions {
+                for input in &transaction.inputs {
+                    self.utxos.remove(&input.prev_transaction_output_hash);
+                }
+                for output in transaction.outputs.iter() {
+                    self.utxos.insert(transaction.hash(), output.clone());
+                }
+            }
+        }
     }
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Block {
     pub header: BlockHeader,
     pub transactions: Vec<Transaction>,
@@ -23,32 +75,76 @@ impl Block {
             transactions: transactions,
         }
     }
-    pub fn hash(&self) -> ! {
-        unimplemented!()
+    pub fn hash(&self) -> Hash {
+        Hash::hash(self)
+    }
+    pub fn verify_transactions(
+        &self,
+        predicted_block_height: u64,
+        utxos: &HashMap<Hash, TransactionOutput>,
+    ) -> Result<()> {
+        let mut inputs: HashMap<Hash, TransactionOutput> = HashMap::new();
+        // reject completely empty blocks
+        if self.transactions.is_empty() {
+            return Err(BtcError::InvalidTransaction);
+        }
+        // verify coinbase transaction
+        self.verify_coinbase_transaction(predicted_block_height, utxos)?;
+        for transaction in &self.transactions {
+            let mut input_value = 0;
+            let mut output_value = 0;
+            for input in transaction.inputs.iter().skip(1) {
+                let prev_output = utxos.get(&input.prev_transaction_output_hash);
+                if prev_output.is_none() {
+                    return Err(BtcError::InvalidTransaction);
+                }
+
+                let prev_output = prev_output.unwrap();
+                // prevent same block double spending
+                if input.contains_key(&input.prev_transaction_output_hash) {
+                    return Err(BtcError::InvalidTransaction);
+                }
+                //check if the signature is valid
+                if !input
+                    .signature
+                    .verify(&input.prev_transaction_output_hash, &prev_output.pubkey)
+                {
+                    return Err(BtcError::InvalidSignature);
+                }
+            }
+            for output in &transaction.outputs {
+                output_value += output.value;
+            }
+            if input_value < output_value {
+                return Err(BtcError::InvalidTransaction);
+            }
+        }
+        Ok(())
     }
 }
 
 use crate::U256;
 
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct BlockHeader {
     /// Timestamp of the block
-    pub timestamp: u64,
+    pub timestamp: DateTime<Utc>,
     /// Nonce used to mine the block
     pub nonce: u64,
     /// Hash of the previous block
-    pub prev_block_hash: [u8; 32],
+    pub prev_block_hash: Hash,
     /// Merkle root of the block'stransactions
-    pub merkle_root: [u8; 32],
+    pub merkle_root: MerkleRoot,
     /// target
     pub target: U256,
 }
 
 impl BlockHeader {
     pub fn new(
-        timestamp: u64,
+        timestamp: DateTime<Utc>,
         nonce: u64,
-        prev_block_hash: [u8; 32],
-        merkle_root: [u8; 32],
+        prev_block_hash: Hash,
+        merkle_root: MerkleRoot,
         target: U256,
     ) -> Self {
         BlockHeader {
@@ -59,15 +155,44 @@ impl BlockHeader {
             target,
         }
     }
-    pub fn hash(&self) -> ! {
-        unimplemented!()
+    pub fn hash(&self) -> Hash {
+        Hash::hash(self)
     }
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Transaction {
     pub inputs: Vec<TransactionInput>,
     pub outputs: Vec<TransactionOutput>,
 }
 
-pub struct TransactionInput;
-pub struct TransactionOutput;
+impl Transaction {
+    pub fn new(inputs: Vec<TransactionInput>, outputs: Vec<TransactionOutput>) -> Self {
+        Transaction {
+            inputs: inputs,
+            outputs: outputs,
+        }
+    }
+    pub fn hash(&self) -> Hash {
+        Hash::hash(self)
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct TransactionInput {
+    pub prev_transaction_output_hash: Hash,
+    pub signature: Signature, // dummy have to replace
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct TransactionOutput {
+    pub value: u64,
+    pub unique_id: Uuid,
+    pub pubkey: PublicKey, // dummy have to replace
+}
+
+impl TransactionOutput {
+    pub fn hash(&self) -> Hash {
+        Hash::hash(self)
+    }
+}
